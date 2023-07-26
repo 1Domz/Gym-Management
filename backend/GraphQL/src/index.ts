b53a33bcf0962532ee74f826e96ca40d00743f45
1Domz <dominiquesimbillo03@gmail.com>
2023-07-26 13:25:02 +0800

    change the flow into express instead of using standalone

--- backend/GraphQL/src/index.ts
+++ backend/GraphQL/src/index.ts
@@ -1,22 +1,56 @@
+// import { ApolloServer } from '@apollo/server'
+// import { startStandaloneServer } from '@apollo/server/standalone'
+// import 'dotenv/config.js'
+// import mongoose from 'mongoose'
+// import typeDefs from './schema.js'
+// import resolvers from './resolvers.js'
+
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
-import 'dotenv/config.js'
 import mongoose from 'mongoose'
 import typeDefs from './schema.js'
 import resolvers from './resolvers.js'
+import 'dotenv/config.js'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express'
+import http from 'http'
+import pkg  from 'body-parser'
+import cors from 'cors'
+
+const { json} = pkg
+
+
 main().catch((err) => console.log(err))
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URI)
 
+  const app = express()
+  const httpServer = http.createServer(app)
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
   })
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 8000 },
-  })
+  await server.start()
 
-  console.log(`🚀  Server ready at: ${url}`)
+  app.use(
+    '/gym-management',
+    cors<cors.CorsRequest>({
+      origin: [ 'http://localhost:3000']
+    }),
+    json(),
+    expressMiddleware(server)
+  )
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 8000}, resolve))
+  console.log(`🚀 Server ready at http://localhost:8000/gym-management`)
+
+  // const { url } = await startStandaloneServer(server, {
+  //   listen: { port: 8000 },
+  // })
+
+  // console.log(`🚀  Server ready at: ${url}`)
 }
