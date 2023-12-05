fa49c7a567b09f5a39e977d4eb05a5b387240374
1Domz <dominiquesimbillo03@gmail.com>
2023-12-05 17:47:30 +0800

    added payload to schema for login

--- backend/api/src/schema.ts
+++ backend/api/src/schema.ts
@@ -1,10 +1,16 @@
 const typeDefs = `#graphql
 
   type User {
+    _id: ID!
     firstname: String!
     lastname: String!
     email: String!
     password: String!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
   }
 
   type Query {
@@ -13,6 +19,7 @@ const typeDefs = `#graphql
 
   type Mutation {
     Signup(firstname: String!, lastname: String!, email: String!, password: String!): User
+    Login(email: String!, password: String!): AuthPayload
   }
 
   `
